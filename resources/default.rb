b8536bb6ae10f9f29f6be838101491c7bd704690
Thermatix <mbeckerwork@gmail.com>
2014-11-05 11:45:05 +0000

    fixes typo

--- resources/default.rb
+++ resources/default.rb
@@ -5,7 +5,7 @@ attribute :source, :kind_of => String
 attribute :version, :kind_of => String
 attribute :args, :kind_of => String
 attribute :options, :kind_of => Hash, :default => {}
-attribute :user, :kind_of => string, :default => nil
+attribute :user, :kind_of => String, :default => nil
 
 def initialize(*args)
   super
