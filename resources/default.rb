3018c6a7db85c93acf79ccd3efe4b6e31999a2d7
Thermatix <mbeckerwork@gmail.com>
2014-11-05 14:47:40 +0000

    changes user atribute again

--- resources/default.rb
+++ resources/default.rb
@@ -5,10 +5,10 @@ attribute :source, :kind_of => String
 attribute :version, :kind_of => String
 attribute :args, :kind_of => String
 attribute :options, :kind_of => Hash, :default => {}
-
+attribute :user, :kind_of => String
 def initialize(*args)
   super
   @action = :install
 end
 
-attr_accessor :exists, :upgradeable, :user
+attr_accessor :exists, :upgradeable
