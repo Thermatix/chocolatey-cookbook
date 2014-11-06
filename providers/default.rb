84565450f57554e96829330077fe7e72f5860989
Thermatix <mbeckerwork@gmail.com>
2014-11-06 18:02:52 +0000

    fixes typo

--- providers/default.rb
+++ providers/default.rb
@@ -123,7 +123,7 @@ def install(name)
 end
 
 def upgrade(name)
-  Chef::Log.debug "user: @current_resource.user"
+  Chef::Log.debug "user: #{@current_resource.user}"
   execute "updating #{name} to latest" do
     user @current_resource.user  if @current_resource.user
     command "#{::ChocolateyHelpers.chocolatey_executable} update #{name} #{cmd_args}"
